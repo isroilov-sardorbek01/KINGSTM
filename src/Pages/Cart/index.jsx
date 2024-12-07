20220276f068572098d6f0e6dc433cf4bf3d25b3
isroilov-sardor <notfalse01@gmail.com>
2024-12-07 16:02:40 +0500

    done

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -22,34 +22,40 @@ function Cart() {
     }
   }
   return (
-    <div>
-      <h2>Shopping Cart</h2>
-      <hr />
-      {cartPr.length > 0 ? (
-        <div>
-          {cartPr.map((item, index) => (
-            <div key={index}>
-              <img src={item.pr.image} alt={item.pr.title} width={100} />
-              <h3>{item.pr.title}</h3>
-              <p>{item.pr.category}</p>
-              <p>${item.pr.price}</p>
-              <p>Amount: {item.pr.mount}</p>
-              <div className="color" style={{
-                userSelect: 'none',
-                width: '20px',
-                padding: '10px',
-                borderRadius: '50%',
-                backgroundColor: item.selectedColor,
-                cursor: 'pointer'
-              }}>
-                <button onClick={(e) => { handleDel(e, item.pr.id) }}>remove</button>
+    <div className='cart'>
+      <div className="container cart-container">
+        <h2>Shopping Cart</h2>
+        <hr />
+        <div className="plusWr">
+        {cartPr.length > 0 ? (
+          <div>
+            {cartPr.map((item, index) => (
+              <div className='' key={index}>
+                <img src={item.pr.image} alt={item.pr.title} width={100} />
+                <h3>{item.pr.title}</h3>
+                <p>{item.pr.category}</p>
+                <p>${item.pr.price}</p>
+                <p>Amount: {item.pr.mount}</p>
+                <div className="color" style={{
+                  content: '',
+                  userSelect: 'none',
+                  width: '20px',
+                  padding: '5px',
+                  borderRadius: '50%',
+                  backgroundColor: item.selectedColor,
+                  cursor: 'pointer',
+                  marginBottom: '20px'
+                }}>
+                  <button onClick={(e) => { handleDel(e, item.pr.id) }}>remove</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
         </div>
-      ) : (
-        <p>Your cart is empty.</p>
-      )}
+      </div>
     </div>
   );
 }
